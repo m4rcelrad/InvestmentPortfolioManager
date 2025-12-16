b737d834bb0574048ef73243a4a108a402b1e4c0
m4rcelrad <iamnotmarcel@gmail.com>
2025-12-16 16:09:21 +0100

    utworzenie struktury projektu

---
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using InvestmentPortfolioManager.Core;
+using InvestmentPortfolioManager.Core.Models;
+
+namespace InvestmentPortfolioManager.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Asset a = new();
+            Console.WriteLine("dziala");
+        }
+    }
+}
---
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.Core/Models/Asset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentPortfolioManager.Core.Models
+{
+    public class Asset
+    {
+        public string Name { get; set; }
+    }
+}
