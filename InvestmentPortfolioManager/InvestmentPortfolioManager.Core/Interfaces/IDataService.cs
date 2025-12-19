dcc941f236352c7732a0d44a745e1ad564e93bbf
m4rcelrad <iamnotmarcel@gmail.com>
2025-12-19 15:39:54 +0100

    chore: przygotowanie struktury pod implementację bazy danych

---
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.Core/Interfaces/IDataService.cs
@@ -0,0 +1,20 @@
+﻿using InvestmentPortfolioManager.Core.Enums;
+using InvestmentPortfolioManager.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentPortfolioManager.Core.Interfaces
+{
+    public interface IDataService
+    {
+        void SavePortfolio(List<Asset> assets);
+        
+        List<Asset> LoadPortfolio();
+
+        List<Asset> GetFilteredAssets(double? minPrice, double? maxPrice, RiskEnum? riskLevel, string? nameFragment);
+
+    }
+}
