81f70f351dc6a7464747ec21a079511ef5c139c8
Bartosz Opioła <bopiola@student.agh.edu.pl>
2026-01-14 20:33:37 +0100

    Dodałem testy jedostkowe i jedną funkcję do asset żeby zmienić cenę do testu

---
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.Tests/EventTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InvestmentPortfolioManager.Core.Models;
+
+namespace InvestmentPortfolioManager.Tests
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        public void OnCriticalDrop_ShouldFire_WhenPriceDropsBelowThreshold()
+        {
+            var crypto = new Cryptocurrency("Bitcoin", "BTC", 1, 50000);
+            crypto.LowPriceThreshold = 40000; 
+            bool eventFired = false;
+
+            crypto.OnCriticalDrop += (sym, price, msg) => { eventFired = true; };
+
+            crypto.UpdatePrice(30000);
+
+            Assert.IsTrue(eventFired, "Zdarzenie OnCriticalDrop nie zostało wywołane po spadku ceny!");
+        }
+    }
+}
---
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.Tests/SimulationTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InvestmentPortfolioManager.Core;
+using InvestmentPortfolioManager.Core.Models;
+using System;
+
+namespace InvestmentPortfolioManager.Tests
+{
+    [TestClass]
+    public class SimulationTests
+    {
+        private InvestmentPortfolio _portfolio;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _portfolio = new InvestmentPortfolio();
+        }
+
+        [TestMethod]
+        public void UpdateMarketPrices_Bond_ShouldIncreaseDeterministically()
+        {
+            double initialPrice = 100.0;
+            var bond = new Bond("Gov Bond", "US", 1, initialPrice, 0.05);
+            _portfolio.AddNewAsset(bond);
+
+            _portfolio.UpdateMarketPrices(DateTime.Now.AddDays(1));
+
+            Assert.IsTrue(bond.CurrentPrice > initialPrice, "Cena obligacji powinna wzrosnąć.");
+            Assert.AreNotEqual(initialPrice, bond.CurrentPrice);
+        }
+
+        [TestMethod]
+        public void UpdateMarketPrices_Stock_ShouldNotBeNaN_Or_Infinity()
+        {
+            var stock = new Stock("Tech Corp", "TCH", 1, 100.0);
+            _portfolio.AddNewAsset(stock);
+            for (int i = 0; i < 10; i++)
+            {
+                _portfolio.UpdateMarketPrices(DateTime.Now.AddDays(i));
+            }
+            Assert.IsFalse(double.IsNaN(stock.CurrentPrice), "Cena akcji nie może być NaN.");
+            Assert.IsFalse(double.IsInfinity(stock.CurrentPrice), "Cena akcji nie może być nieskończonością.");
+            Assert.IsTrue(stock.CurrentPrice >= 0, "Cena akcji nie może być ujemna.");
+        }
+    }
+}
