50db16af01fdb91cfc776a886197706000565069
pawszu908 <pawszu@student.agh.edu.pl>
2026-01-14 20:47:26 +0100

    Zainstalowanie i zastosowanie pakietu Entity Framework 6 oraz utworzenie InvestmentPortfolioDbContext - do bazy

---
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.Data/InvestmentPortfolioDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using InvestmentPortfolioManager.Core.Models;
+{
+    
+}
+
+namespace InvestmentPortfolioManager.Data
+{
+    public class InvestmentPortfolioDbContext : DbContext
+    {
+        public DbSet<InvestmentPortfolio> Portfolios { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Bond> Bonds { get; set; }
+        public DbSet<Cryptocurrency> Cryptocurrencies { get; set; }
+        public DbSet<RealEstate> RealEstates { get; set; }
+        public DbSet<Commodity> Commodities { get; set; }
+
+    }
+}
