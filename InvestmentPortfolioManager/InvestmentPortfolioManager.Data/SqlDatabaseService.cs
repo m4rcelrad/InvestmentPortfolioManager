7ae186553c1809cad77dad240bf6087beb8f3599
pawszu908 <pawszu@student.agh.edu.pl>
2026-01-16 22:21:45 +0100

    Poprawiono metodę zapisywania portfela - nadpisuje zmiany w istniejacych portfelach, a nie tworzy nowe encje. Napisano metode filtrowania aktyw

--- InvestmentPortfolioManager/InvestmentPortfolioManager.Data/SqlDatabaseService.cs
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.Data/SqlDatabaseService.cs
@@ -1,12 +1,13 @@
-﻿using InvestmentPortfolioManager.Core.Interfaces;
-using InvestmentPortfolioManager.Core;
+﻿using InvestmentPortfolioManager.Core;
+using InvestmentPortfolioManager.Core.Enums;
+using InvestmentPortfolioManager.Core.Interfaces;
 using InvestmentPortfolioManager.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using InvestmentPortfolioManager.Core.Enums;
 
 
 
@@ -16,7 +17,37 @@ namespace InvestmentPortfolioManager.Data
     {
         public List<Asset> GetFilteredAssets(InvestmentPortfolio portfolio, double? minPrice, double? maxPrice, RiskEnum? riskLevel, string? nameFragment)
         {
-            throw new NotImplementedException();
+            if (portfolio == null)
+                throw new ArgumentNullException(nameof(portfolio));
+
+            using (var db = new InvestmentPortfolioDbContext())
+            {
+                // aktywa z bazy danych bez poziomu ryzyka (to co da się przefiltrować w SQL)
+                var query = db.Portfolios
+                    .Where(p => p.InvestmentPortfolioId == portfolio.InvestmentPortfolioId)
+                    .SelectMany(p => p.Assets);
+
+                if (minPrice.HasValue)
+                    query = query.Where(a => a.CurrentPrice >= minPrice.Value);
+
+                if (maxPrice.HasValue)
+                    query = query.Where(a => a.CurrentPrice <= maxPrice.Value);
+
+                if (!string.IsNullOrWhiteSpace(nameFragment))
+                    query = query.Where(a =>
+                        a.AssetName.Contains(nameFragment) ||
+                        a.AssetSymbol.Contains(nameFragment));
+
+                // przejscie do pamięci, bo ryzyko trzeba filtrować tam - osobna metoda GetRiskAssessment()
+                var assets = query.ToList();
+
+                if (riskLevel.HasValue)
+                    assets = assets
+                        .Where(a => a.GetRiskAssessment() == riskLevel.Value)
+                        .ToList();
+
+                return assets;
+            }
         }
 
         public List<InvestmentPortfolio> LoadAllPortfolios()
@@ -35,13 +66,18 @@ namespace InvestmentPortfolioManager.Data
 
         public void SavePortfolios(List<InvestmentPortfolio> portfolios)
         {
+            Console.WriteLine("Zapisywanie portfeli do bazy danych...");
             using (var db = new InvestmentPortfolioDbContext())
             {
-                Console.WriteLine("Przygotowywanie portfeli do zapisania do bazy danych...");
-                db.Portfolios.AddRange(portfolios);
+                foreach (var portfolio in portfolios)
+                {
+                    db.Portfolios.AddOrUpdate(portfolio);
+                }
+
                 db.SaveChanges();
-                Console.WriteLine("Portfele zostały zapisane do bazy danych.");
             }
+
+            Console.WriteLine("Portfele zostały zapisane do bazy danych.");
         }
     }
 }
