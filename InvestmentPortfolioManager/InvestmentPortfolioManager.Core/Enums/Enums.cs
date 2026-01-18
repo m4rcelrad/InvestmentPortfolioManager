f86ba701e8cda2c811aeb427f24d4f714c846d87
Bartosz Opioła <bopiola@student.agh.edu.pl>
2026-01-18 22:31:15 +0100

    Dodanie komentarzy do dokumentacji XML do części klas

--- InvestmentPortfolioManager/InvestmentPortfolioManager.Core/Enums/Enums.cs
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.Core/Enums/Enums.cs
@@ -6,23 +6,75 @@ using System.Threading.Tasks;
 
 namespace InvestmentPortfolioManager.Core.Enums
 {
-   public enum RiskEnum
+    /// <summary>
+    /// Określa poziom ryzyka inwestycyjnego dla danego aktywa.
+    /// </summary>
+    public enum RiskEnum
     {
+        /// <summary>
+        /// Niskie ryzyko (np. obligacje skarbowe, stabilne fundusze).
+        /// </summary>
         Low,
+
+        /// <summary>
+        /// Średnie ryzyko (np. akcje blue chip, zrównoważone portfele).
+        /// </summary>
         Medium,
+
+        /// <summary>
+        /// Wysokie ryzyko (np. akcje rynków wschodzących, agresywne fundusze).
+        /// </summary>
         High,
+
+        /// <summary>
+        /// Ekstremalnie wysokie ryzyko (np. kryptowaluty, derywaty, penny stocks).
+        /// </summary>
         ExtremelyHigh
     }
 
+    /// <summary>
+    /// Jednostki miary używane dla surowców i towarów (Commodities).
+    /// </summary>
     public enum UnitEnum
     {
+        /// <summary>
+        /// Uncja (np. dla złota, srebra).
+        /// </summary>
         Ounce,
+
+        /// <summary>
+        /// Baryłka (np. dla ropy naftowej).
+        /// </summary>
         Barrel,
+
+        /// <summary>
+        /// Tona (np. dla węgla, stali).
+        /// </summary>
         Ton,
+
+        /// <summary>
+        /// Kilogram.
+        /// </summary>
         Kilogram,
+
+        /// <summary>
+        /// Gram.
+        /// </summary>
         Gram,
+
+        /// <summary>
+        /// Buszel (np. dla zbóż: pszenica, kukurydza).
+        /// </summary>
         Bushel,
+
+        /// <summary>
+        /// Litr.
+        /// </summary>
         Liter,
+
+        /// <summary>
+        /// Megawatogodzina (np. dla energii elektrycznej).
+        /// </summary>
         MWh
     }
 }
--- InvestmentPortfolioManager/InvestmentPortfolioManager.Core/Interfaces/IAsset.cs
+++ InvestmentPortfolioManager/InvestmentPortfolioManager.Core/Interfaces/IAsset.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 
 namespace InvestmentPortfolioManager.Core.Interfaces
 {
+    /// <summary>
+    /// Definiuje podstawowe zachowania dla każdego aktywa w systemie.
+    /// </summary>
     internal interface IAsset
     {
+        /// <summary>
+        /// Symuluje zmianę ceny aktywa w czasie na podstawie zdefiniowanej zmienności i trendu.
+        /// </summary>
+        /// <param name="simulationDate">Data, dla której generowana jest nowa cena.</param>
         void SimulatePriceChange(DateTime simulationDate);
+
+        /// <summary>
+        /// Zwraca ocenę ryzyka inwestycyjnego dla danego typu aktywa.
+        /// </summary>
+        /// <returns>Wartość z enum <see cref="RiskEnum"/> określająca poziom ryzyka.</returns>
         RiskEnum GetRiskAssessment();
     }
 }
